8330ed0764627358ae9f35195e577ed4f7b700b0
GUSTAVO PADRAO DI IORIO AGUIAR <gpiorio@gmail.com>
2022-11-12 15:38:43 -0300

    Create imagem.js

---
+++ formacao_iniciante_programacao/jogo_Freeway/imagem.js
@@ -0,0 +1,15 @@
+//Variaveis para imagem
+let estradaBackgroun;
+let ator1;
+let carro1;
+let carro2;
+let carro3;
+
+function preload(){
+    estradaBackgroun = loadImage("imagens/estrada.png");
+    ator1 = loadImage("imagens/ator-1.png");
+    carro1 = loadImage("imagens/carro-1.png");
+    carro2 = loadImage("imagens/carro-2.png");
+    carro3 = loadImage("imagens/carro-3.png");
+    imgCar = [carro1, carro2, carro3, carro3, carro2, carro1]
+}
